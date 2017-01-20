05b561e5b3cd2e2f648ba9f40abdde5744988f90
Qian <pandachain731@gmail.com>
2017-01-19 19:57:17 -0800

    add HomePage and AboutPage components

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Course review</h1>
+        <p>Some text here</p>
+        <Link to="about" className="btn btn-primary btn-large">Learn more</Link>
+      </div>
+    )
+  }
+}
+
+export default HomePage
