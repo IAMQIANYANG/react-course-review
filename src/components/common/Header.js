c5da42f67c5346bef71cc57c6670f67b0654ab7f
Qian <pandachain731@gmail.com>
2017-01-19 21:30:14 -0800

    add CoursesPage and route

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
 
 const Header = () => {
   return (
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
-      {"|"}
-      <Link to="/about" activeClassName="active">About</Link>
+      {" | "}
+      <Link to="courses" activeClassName="active">Courses</Link>
+      {" | "}
+      <Link to="about" activeClassName="active">About</Link>
     </nav>
   )
 };
