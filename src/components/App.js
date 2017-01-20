c8d19cffff1dfcb976dc675fc695e77f75985eb2
Qian <pandachain731@gmail.com>
2017-01-19 20:43:43 -0800

    add routes and App.js

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, {Componet, PropTypes} from 'react';
+
+class App extends Componet {
+  render() {
+    return (
+      <div className="container-fluid">
+        <p>Header here...</p>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default App
